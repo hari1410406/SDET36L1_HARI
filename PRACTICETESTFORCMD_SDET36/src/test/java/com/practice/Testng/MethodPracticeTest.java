617b724f5aac4fb5f4afb732734e77bf85353473
hari1410406 <103132602+hari1410406@users.noreply.github.com>
2022-07-19 15:28:11 +0530

    Update MethodPracticeTest.java

--- PRACTICETESTFORCMD_SDET36/src/test/java/com/practice/Testng/MethodPracticeTest.java
+++ PRACTICETESTFORCMD_SDET36/src/test/java/com/practice/Testng/MethodPracticeTest.java
@@ -11,12 +11,14 @@ public class MethodPracticeTest {
 	public void allTestngTests() {
 		Reporter.log("this is test class 2", true);
 		//this is another pull request for checking to merged or not 
+		//Second time Repository changes
 	}
 	
 	@Test
 	public void allTest() {
 		Reporter.log("this is another method",true);
 		//this change for pull request
+		//pull request cannot pushed
 	}
 
 }
